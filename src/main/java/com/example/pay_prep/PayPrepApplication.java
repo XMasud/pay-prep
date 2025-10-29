21268c1a6d89106c7a77b0a3791e7b2dab2d2999
Masud Rana <masud.aiub.cse@gmail.com>
2025-10-29 21:06:12 +0100

    Prep initatl config added

---
+++ src/main/java/com/example/pay_prep/PayPrepApplication.java
@@ -0,0 +1,13 @@
+package com.example.pay_prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayPrepApplication.class, args);
+	}
+
+}
