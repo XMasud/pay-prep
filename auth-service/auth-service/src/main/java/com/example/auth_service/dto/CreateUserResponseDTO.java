5c0a934ac1d4978a7b7cc07df1e092d65d15b3b3
Masud Rana <masud.aiub.cse@gmail.com>
2025-11-17 23:10:38 +0100

    auth service added

---
+++ auth-service/auth-service/src/main/java/com/example/auth_service/dto/CreateUserResponseDTO.java
@@ -0,0 +1,19 @@
+package com.example.auth_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreateUserResponseDTO {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+}
---
+++ auth-service/auth-service/src/main/java/com/example/auth_service/model/User.java
@@ -0,0 +1,29 @@
+package com.example.auth_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull(message = "Password is required")
+    private String password;
+}
